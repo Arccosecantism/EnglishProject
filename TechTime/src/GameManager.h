b151f6b8d74ec22f7401c45f0a1fd5b5d3062c46
xx_Archibald_xx <riley.d.waugh@gmail.com>
2015-05-12 13:06:12 -0700

    Adding Q/A reading from a file

--- TechTime/src/GameManager.h
+++ TechTime/src/GameManager.h
@@ -9,10 +9,12 @@ public:
     GameManager();
     void update(ofVec2f& mousePos, bool& clicked, bool& pressed);
     void draw();
+    void fillQATexts(std::string filename);
 
 
 private:
     AnswerManager answerMan;
     QuestionManager questionMan;
     ofTexture GameBG;
+    std::vector<std::vector<std:string>> QASetTexts
 };
